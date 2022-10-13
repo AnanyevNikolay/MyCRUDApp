0f2ecea627ed60efd07676ea18399e152f8b7138
NikolayAnanyev <ananievnikolai@mail.ru>
2022-10-13 17:23:06 +0400

    Completed exercise 2.3.1

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,51 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    public User() {
+    }
+
+    public User(String name, String last_name) {
+        this.name = name;
+        this.last_name = last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+}
